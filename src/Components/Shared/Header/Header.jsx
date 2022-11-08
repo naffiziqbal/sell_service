ade994e4e0e8d941f12b8325cb70f4c47218883e
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-08 23:39:03 +0600

    footer

--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -66,7 +66,7 @@ const Header = () => {
           </ul>
         </div>
         <Link to="/" className="btn btn-ghost normal-case text-xl">
-          CameraWala
+          CinemaWala
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
