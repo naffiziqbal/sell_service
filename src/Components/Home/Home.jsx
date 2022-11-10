2fab3702ab405ecbc6891bbe23d7ac9683b49d37
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-10 11:22:27 +0600

    Loader failed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,18 +11,6 @@ const Home = () => {
   const { loading, setLoading } = useContext(AuthContext);
   useTitle("Home");
 
-  if (loading) {
-    return (
-      <div class="flex items-center justify-center">
-        <div
-          class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
-          role="status"
-        >
-          <span class="visually-hidden">...</span>
-        </div>
-      </div>
-    );
-  }
   return (
     <div>
       <div className="mb-5">
